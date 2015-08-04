e6dc524c9acc0edebc4f9c5339e56333c4fac4bb
mick90207 <b98502030@ntu.edu.tw>
2015-08-05 07:25:27 +0800

    One Motor Output is okay

--- Motor_Control_CPP/Motor_Control_CPP.cpp
+++ Motor_Control_CPP/Motor_Control_CPP.cpp
@@ -3,24 +3,41 @@
 #include "Visual.h"
 #include <string> 
 #include <iostream>
+#include <thread>
 #include "Packet.h"
 
 using namespace std;
+void data_receive();
+SerialPort *serial = new SerialPort();
+Packet packet;
+
 int _tmain(int argc, _TCHAR* argv[])
 {
-	SerialPort *serial = new SerialPort();
-	serial->serial_port_name = "COM4";
+	serial->serial_port_name = "\\\\.\\COM4";
 	serial->BaudRate = 4900000;
 	serial->Open_SerialPort();
-	Packet packet;
+
 	packet.PacketType = 0x12;
 	packet.Ch_Status = 0x00;
 	packet.DataLength = 0x02;
 	packet.Data[0] = 0x0f;
 	packet.CRC8 = 0xf2;
 	serial->Write_into_SerialPort(packet);
+	thread mThread(data_receive);
+	while (true){
 
+	}
+	
+	
+	std::terminate();
+	cout << "¥ô·NÁäµ²§ô" << endl;
 	system("pause");
 	return 0;
 }
 
+void data_receive(){
+	while (true){
+		//serial->Write_into_SerialPort(packet);
+		serial->Read_Data();
+	}
+}
--- Motor_Control_CPP/Serial_Port.h
+++ Motor_Control_CPP/Serial_Port.h
@@ -8,13 +8,18 @@
 #define HEADER_Packet
 
 #include "Packet.h"
+#include "AHRS.h"
 class Packet;
 
 class SerialPort{
 
 public:
+	static const int Size_Buffer = 30000;
+	char szBuff[Size_Buffer];//Size_Buffer
+	int MAX_PACKET_SIZE = 100;
 	std::string serial_port_name;
 	DWORD BaudRate;
+	DWORD Read_Ptr=0;
 	COMMTIMEOUTS timeouts;
 	DCB dcbSerialParams;
 	HANDLE hSerial;
@@ -23,6 +28,7 @@ public:
 	void SetTimeout_SerialPort();
 	void Close_SerialPort();
 	bool Write_into_SerialPort(Packet AHRSPacket);
+	bool Read_Data();
 };
 
 
@@ -78,7 +84,8 @@ void SerialPort::Close_SerialPort(){
 }
 
 bool SerialPort::Write_into_SerialPort(Packet AHRSPacket){
-	LPDWORD byteswritten = (LPDWORD) 6 + (LPDWORD)AHRSPacket.DataLength;
+	DWORD byteswritten = 6 + AHRSPacket.DataLength;
+	DWORD NumberOfBytesWritten;
 	int length = 0;
 	uint8_t* packet = new uint8_t[6 + AHRSPacket.DataLength];
 	packet[0] = (uint8_t)'E';
@@ -95,8 +102,254 @@ bool SerialPort::Write_into_SerialPort(Packet AHRSPacket){
 	}
 	packet[6 + i] = AHRSPacket.CRC8;
 
-	return WriteFile(hSerial, packet, (DWORD)byteswritten, (LPDWORD)0, NULL);
-	//return true;
+	return WriteFile(hSerial, packet, byteswritten, &NumberOfBytesWritten , NULL);
+}
+
+bool SerialPort::Read_Data(){
+
+	int continue_parsing = 1;
+	//int bytes_to_read;
+	bool found_packet;
+	int packet_start_index;
+	int packet_index;
+	DWORD   dwErrors;
+	COMSTAT comStat;
+
+	//取得Receive但尚未Read的數目(comStat.cbInQue)
+	ClearCommError(hSerial, &dwErrors, &comStat);
+	//std::cout << dwErrors << ":::" << comStat.cbInQue << std::endl;
+	
+	
+	if (ReadFile(hSerial, szBuff, comStat.cbInQue, &Read_Ptr, NULL)){
+		//std::cout << szBuff << std::endl;
+		//std::cout << dwErrors << ":::" << comStat.cbInQue << std::endl;
+
+
+		while (continue_parsing == 1)//RXbufPtr >= 8 &&
+		{
+			// Search for the packet start sequence
+			found_packet = false;
+			packet_start_index = 0;
+			for (packet_index = 0; packet_index < comStat.cbInQue; packet_index++)
+			{
+				if (szBuff[packet_index] == 'E' && szBuff[packet_index + 1] == 'C' && szBuff[packet_index + 2] == 'S')
+				{
+					found_packet = true;
+					packet_start_index = packet_index;
+
+					break;
+				}
+			}
+
+			// If start sequence found, try to recover all the data in the packet
+			if (found_packet && ((comStat.cbInQue - packet_start_index) >= 8))
+			{
+				int i;
+				Packet *DataPacket = new Packet();
+				DataPacket->PacketType = szBuff[packet_start_index + 3];
+				DataPacket->Ch_Status = szBuff[packet_start_index + 4];
+				DataPacket->DataLength = szBuff[packet_start_index + 5];
+				DataPacket->Data = new byte[DataPacket->DataLength-1];
+
+				// Only process packet if data_size is not too large.
+				if (DataPacket->DataLength <= MAX_PACKET_SIZE)
+				{
+
+					// If a full packet has been received, then the full packet size should be
+					// 3 + 1 + 1 + 1 + [data_size] + 1
+					// that is, 3 bytes for the start sequence, 1 byte for type, 1 byte for status, 1 byte for data length, 
+					// data_size bytes for packet data inculde 1 bytes for the CRC-8.
+					// If enough data has been received, go ahead and recover the packet.  If not, wait until the
+					// rest of the data arrives
+					int buffer_length = (comStat.cbInQue - packet_start_index);
+					int packet_length = (6 + DataPacket->DataLength);
+					if (buffer_length >= packet_length)
+					{
+						if (DataPacket->DataLength == 0)
+						{
+							// this packet length is wrong!!!
+						}
+						else
+						{
+							// A full packet has been received.  Retrieve the data.
+							for (i = 0; i < (DataPacket->DataLength - 1); i++)
+							{
+								DataPacket->Data[i] = szBuff[packet_start_index + 6 + i];
+							}
+							DataPacket->CRC8 = szBuff[packet_start_index + 6 + i];
+							unsigned int x;
+							unsigned char a[4];
+							a[3] = DataPacket->Data[5];
+							a[2] = DataPacket->Data[6];
+							a[1] = DataPacket->Data[7];
+							a[0] = DataPacket->Data[8];
+							x = *(int *)a;
+							//std::cout << "Data Get" << std::endl;
+							printf("%d\n", x);
+							//cout<<handle_packet(DataPacket);
+							//continue_parsing = 0;//Delay的嫌疑犯?
+						}
+
+						// Copy all received bytes that weren't part of this packet into the beginning of the
+						// buffer.  Then, reset RXbufPtr.
+
+						for (int index = 0; index < (buffer_length - packet_length); index++)
+						{
+							szBuff[index] = szBuff[(packet_start_index + packet_length) + index];
+						}
+
+						comStat.cbInQue = (buffer_length - packet_length);
+
+					}
+					else
+					{
+						continue_parsing = 0;
+					}
+				}
+				else
+				{
+					// data_size was too large - the packet data is invalid.  Clear the RX buffer.
+					comStat.cbInQue = 0;
+					continue_parsing = 0;
+					//PacketReceivedEvent(PName.CMD_OVER_DATA_LENGTH, -1, DataPacket.Ch_Status);
+				}
+			}
+			else
+			{
+				continue_parsing = 0;
+			}
+		}
+
+
+		//std::cout << sizeof(szBuff) << std::endl;
+		//error occurred. Report to user.
+		return true;
+	}
+	else{
+		return false;
+	}
+	/*
+	try
+	{
+		bytes_to_read = serialPort.BytesToRead;
+
+		if ((RXbufPtr + bytes_to_read) >= RX_BUF_SIZE)
+		{
+			RXbufPtr = 0;
+		}
+
+		if (bytes_to_read >= RX_BUF_SIZE)
+		{
+			bytes_to_read = RX_BUF_SIZE - 1;
+		}
+
+		// Get serial data
+		serialPort.Read(RXbuffer, RXbufPtr, bytes_to_read);
+		//serialPort.Read(RXbuffer, 0, bytes_to_read);
+
+		RXbufPtr += bytes_to_read;
+	}
+	catch
+	{
+		COMFailedEvent();
+		return;
+	}
+
+	bool found_packet;
+	int packet_start_index;
+	int packet_index;
+
+	// If there are enough bytes in the buffer to construct a full packet, then check data.
+	// There are RXbufPtr bytes in the buffer at any given time
+	while (RXbufPtr >= 8 && (continue_parsing == 1))
+	{
+		// Search for the packet start sequence
+		found_packet = false;
+		packet_start_index = 0;
+		for (packet_index = 0; packet_index < (RXbufPtr - 2); packet_index++)
+		{
+			if (RXbuffer[packet_index] == 'E' && RXbuffer[packet_index + 1] == 'C' && RXbuffer[packet_index + 2] == 'S')
+			{
+				found_packet = true;
+				packet_start_index = packet_index;
+
+				break;
+			}
+		}
+
+		// If start sequence found, try to recover all the data in the packet
+		if (found_packet && ((RXbufPtr - packet_start_index) >= 8))
+		{
+			int i;
+			Packet DataPacket = new Packet();
+			DataPacket.PacketType = RXbuffer[packet_start_index + 3];
+			DataPacket.Ch_Status = RXbuffer[packet_start_index + 4];
+			DataPacket.DataLength = RXbuffer[packet_start_index + 5];
+			DataPacket.Data = new byte[DataPacket.DataLength];
+
+			// Only process packet if data_size is not too large.
+			if (DataPacket.DataLength <= MAX_PACKET_SIZE)
+			{
+
+				// If a full packet has been received, then the full packet size should be
+				// 3 + 1 + 1 + 1 + [data_size] + 1
+				// that is, 3 bytes for the start sequence, 1 byte for type, 1 byte for status, 1 byte for data length, 
+				// data_size bytes for packet data inculde 1 bytes for the CRC-8.
+				// If enough data has been received, go ahead and recover the packet.  If not, wait until the
+				// rest of the data arrives
+				int buffer_length = (RXbufPtr - packet_start_index);
+				int packet_length = (6 + DataPacket.DataLength);
+				if (buffer_length >= packet_length)
+				{
+					if (DataPacket.DataLength == 0)
+					{
+						// this packet length is wrong!!!
+					}
+					else
+					{
+						// A full packet has been received.  Retrieve the data.
+						for (i = 0; i < (DataPacket.DataLength - 1); i++)
+						{
+							DataPacket.Data[i] = RXbuffer[packet_start_index + 6 + i];
+						}
+						DataPacket.CRC8 = RXbuffer[packet_start_index + 6 + i];
+
+						handle_packet(DataPacket);
+
+						//continue_parsing = 0;//Delay的嫌疑犯?
+					}
+
+					// Copy all received bytes that weren't part of this packet into the beginning of the
+					// buffer.  Then, reset RXbufPtr.
+
+					for (int index = 0; index < (buffer_length - packet_length); index++)
+					{
+						RXbuffer[index] = RXbuffer[(packet_start_index + packet_length) + index];
+					}
+
+					RXbufPtr = (buffer_length - packet_length);
+
+				}
+				else
+				{
+					continue_parsing = 0;
+				}
+			}
+			else
+			{
+				// data_size was too large - the packet data is invalid.  Clear the RX buffer.
+				RXbufPtr = 0;
+				continue_parsing = 0;
+				PacketReceivedEvent(PName.CMD_OVER_DATA_LENGTH, -1, DataPacket.Ch_Status);
+			}
+		}
+		else
+		{
+			continue_parsing = 0;
+		}
+	}
+
+	*/
 }
 
 #endif
